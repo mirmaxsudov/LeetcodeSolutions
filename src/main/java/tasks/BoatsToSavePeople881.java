417fea75debf3f7ed5b8c09df89a8009408b90b1
Abdurahmon <abdurahmonmirmaxsudov2804@gmail.com>
2024-05-06 17:35:54 +0500

    commit

--- src/main/java/tasks/BoatsToSavePeople881.java
+++ src/main/java/tasks/BoatsToSavePeople881.java
@@ -1,78 +1,51 @@
 package tasks;
 
 
+import java.util.Arrays;
+
 public class BoatsToSavePeople881 {
     public static void main(String[] args) {
+//        System.out.println(numRescueBoats(
+//                new int[]{1, 2}, 3
+//        ));
+//        System.out.println(numRescueBoats(
+//                new int[]{3, 2, 2, 1}, 3
+//        ));
+//        System.out.println(numRescueBoats(
+//                new int[]{3, 5, 3, 4}, 5
+//        ));
         System.out.println(numRescueBoats(
-                new int[]{1, 2}, 3
-        ));
-        System.out.println(numRescueBoats(
-                new int[]{3, 2, 2, 1}, 3
-        ));
-        System.out.println(numRescueBoats(
-                new int[]{3, 5, 3, 4}, 5
+                new int[]{3, 2, 3, 2, 2}, 6
         ));
     }
 
     public static int numRescueBoats(int[] people, int limit) {
-        int countBoats = 0;
-        int peopleCount = 0;
-        quickSort(people);
+        Arrays.sort(people);
+        int count = 0;
+        boolean[] visited = new boolean[people.length];
 
-        int totalW = 0;
-        for (int i = people.length - 1; i >= 0; i--) {
-            totalW += people[i];
-            peopleCount++;
+        for (int i = people.length - 1; i >= 1; i--) {
+            int firstW = people[i];
+            int secondW = people[i - 1];
 
-            if (peopleCount == 2) {
-                if (totalW == limit) {
-                    totalW = 0;
-                    countBoats++;
-                } else if (totalW > limit) {
-                    i++;
-                    totalW = 0;
-                }
+            if (firstW + secondW > limit) {
+                count++;
+                visited[i] = true;
+                continue;
+            }
+
+            if (firstW + secondW <= limit) {
+                i -= 2;
+                count++;
             }
         }
 
-        return countBoats;
-    }
+        if (visited[0])
+            return count;
 
-    public static void quickSort(int[] array) {
-        if (array == null || array.length == 0) {
-            return;
-        }
-        int length = array.length;
-        quickSort(array, 0, length - 1);
-    }
+        if (visited[1])
+            return count;
 
-    private static void quickSort(int[] array, int left, int right) {
-        if (left >= right) {
-            return;
-        }
-        int pivot = array[(left + right) / 2];
-        int index = partition(array, left, right, pivot);
-
-        quickSort(array, left, index - 1);
-        quickSort(array, index, right);
-    }
-
-    private static int partition(int[] array, int left, int right, int pivot) {
-        while (left <= right) {
-            while (array[left] < pivot)
-                left++;
-
-            while (array[right] > pivot)
-                right--;
-
-            if (left <= right) {
-                int temp = array[left];
-                array[left] = array[right];
-                array[right] = temp;
-                left++;
-                right--;
-            }
-        }
-        return left;
+        return count + 1;
     }
 }
---
+++ src/main/java/tasks/TakeIP.java
@@ -0,0 +1,8 @@
+package tasks;
+
+import java.net.UnknownHostException;
+
+public class TakeIP {
+    public static void main(String[] args) throws UnknownHostException {
+    }
+}
