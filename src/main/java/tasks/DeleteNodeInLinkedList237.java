36983a0e1a98602b453af540a1fc06ded915b981
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2024-11-29 11:48:15 +0500

    commit

---
+++ src/main/java/tasks/DeleteNodeInLinkedList237.java
@@ -0,0 +1,11 @@
+package tasks;
+
+public class DeleteNodeInLinkedList237 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void deleteNode(ListNode node) {
+        
+    }
+}
