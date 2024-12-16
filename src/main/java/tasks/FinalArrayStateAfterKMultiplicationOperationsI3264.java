68f1bd66e9e39ab43011bbecbd3c85b39ab6eec9
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2024-12-16 08:47:52 +0500

    Written sout for that solution

--- src/main/java/tasks/FinalArrayStateAfterKMultiplicationOperationsI3264.java
+++ src/main/java/tasks/FinalArrayStateAfterKMultiplicationOperationsI3264.java
@@ -1,8 +1,10 @@
 package tasks;
 
+import java.util.Arrays;
+
 public class FinalArrayStateAfterKMultiplicationOperationsI3264 {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(getFinalState(new int[]{1, 2, 3, 4}, 2, 3)));
     }
 
     public static int[] getFinalState(int[] nums, int k, int multiplier) {
@@ -10,16 +12,14 @@ public class FinalArrayStateAfterKMultiplicationOperationsI3264 {
             int min = Integer.MAX_VALUE;
             int index = -1;
 
-            for (int j = 0; j < nums.length; j++) {
+            for (int j = 0; j < nums.length; j++)
                 if (nums[j] < min) {
                     min = nums[j];
                     index = j;
                 }
-            }
 
-            if (index != -1) {
+            if (index != -1)
                 nums[index] *= multiplier;
-            }
         }
 
         return nums;
