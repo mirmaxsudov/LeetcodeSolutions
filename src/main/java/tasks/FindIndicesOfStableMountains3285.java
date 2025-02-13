603255d8565d2e00c1ea6e7e29e9b77cdd41b4a2
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-02-13 18:27:07 +0500

    solved #3285

---
+++ src/main/java/tasks/FindIndicesOfStableMountains3285.java
@@ -0,0 +1,32 @@
+package tasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FindIndicesOfStableMountains3285 {
+    public static void main(String[] args) {
+        System.out.println(stableMountains(new int[]{1, 2, 3, 4, 5}, 2));
+    }
+
+    public static List<Integer> stableMountainsVer2(int[] height, int threshold) {
+        return IntStream.range(0, height.length - 1)
+                .filter(i -> height[i] > threshold)
+                .map(i -> i + 1)
+                .boxed()
+                .toList();
+    }
+
+    public static List<Integer> stableMountains(int[] height, int threshold) {
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < height.length - 1; i++)
+            if (height[i] > threshold)
+                res.add(i + 1);
+
+
+        return res;
+    }
+}
