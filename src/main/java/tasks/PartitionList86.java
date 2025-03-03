564489d5946b563541535aca3107a3ed1c00102c
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-03-03 11:22:08 +0500

    Solved #86

--- src/main/java/tasks/PartitionList86.java
+++ src/main/java/tasks/PartitionList86.java
@@ -2,19 +2,48 @@ package tasks;
 
 public class PartitionList86 {
     public static void main(String[] args) {
+//        int[] arr = {1, 4, 3, 2, 5, 2};
+        int[] arr = {2, 1};
 
+        ListNode res = new ListNode(-1);
+        ListNode temp = res;
+
+        for (int num : arr) {
+            temp.next = new ListNode(num);
+            temp = temp.next;
+        }
+
+        res = res.next;
+
+        ListNode result = partition(res, 3);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
     }
 
     public static ListNode partition(ListNode head, int x) {
         ListNode res = new ListNode(-1);
+        ListNode temp = res;
 
-        boolean isGreaterFromX = false;
+        ListNode helpToCollectGreater = new ListNode(-1);
+        ListNode tempHelp = helpToCollectGreater;
 
         while (head != null) {
-            var val = head.val;
+            int val = head.val;
 
+            if (val >= x) {
+                tempHelp.next = new ListNode(val);
+                tempHelp = tempHelp.next;
+            } else {
+                temp.next = new ListNode(val);
+                temp = temp.next;
+            }
+            head = head.next;
         }
 
+        temp.next = helpToCollectGreater.next;
         return res.next;
     }
 }
--- src/main/java/tasks/RearrangeArrayElementsBySign2149.java
+++ src/main/java/tasks/RearrangeArrayElementsBySign2149.java
@@ -4,7 +4,25 @@ import java.util.Arrays;
 
 public class RearrangeArrayElementsBySign2149 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(rearrangeArray(new int[]{3, 1, -2, -5, 2, -4})));
+        System.out.println(Arrays.toString(rearrangeArrayVer2(new int[]{3, 1, -2, -5, 2, -4})));
+    }
+
+    public static int[] rearrangeArrayVer2(int[] nums) {
+        int[] res = new int[nums.length];
+        int pIndex = 0;
+        int nIndex = 1;
+
+        for (int num : nums) {
+            if (num >= 0) {
+                res[pIndex] = num;
+                pIndex += 2;
+            } else {
+                res[nIndex] = num;
+                nIndex += 2;
+            }
+        }
+
+        return res;
     }
 
     public static int[] rearrangeArray(int[] nums) {
