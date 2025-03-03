740421478e5772a0b2f3f9f04a3b82695a20f324
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-03-03 16:06:59 +0500

    Solved #3467

---
+++ src/main/java/tasks/LargestNumberAfterDigitSwapsByParity2231.java
@@ -0,0 +1,12 @@
+package tasks;
+
+public class LargestNumberAfterDigitSwapsByParity2231 {
+    public static void main(String[] args) {
+        System.out.println(largestInteger(1234));
+    }
+
+    public static int largestInteger(int num) {
+
+        return -1;
+    }
+}
---
+++ src/main/java/tasks/TransformArrayByParity3467.java
@@ -0,0 +1,22 @@
+package tasks;
+
+public class TransformArrayByParity3467 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] transformArray(int[] nums) {
+        int[] res = new int[nums.length];
+        int l = 0, r = nums.length - 1;
+
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                res[l++] = 0;
+            } else {
+                res[r--] = 1;
+            }
+        }
+
+        return res;
+    }
+}
