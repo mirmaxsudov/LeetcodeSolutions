b7a86ba77bf17a6f1f1ecc575c9a05d59a4064cc
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-05-23 20:20:08 +0500

    Added

---
+++ src/main/java/univer_lesson/exam/M1.java
@@ -0,0 +1,23 @@
+package univer_lesson.exam;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class M1 {
+    static class BinaryNumbers {
+        public List<String> generate(int N) {
+            List<String> res = new ArrayList<>();
+            Queue<String> q = new LinkedList<>();
+            q.add("1");
+            for (int i = 0; i < N; i++) {
+                String s = q.poll();
+                res.add(s);
+                q.add(s + "0");
+                q.add(s + "1");
+            }
+            return res;
+        }
+    }
+}
