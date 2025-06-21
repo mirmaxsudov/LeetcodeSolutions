804d29e2acfcf62806465191f7f91af01fe9c3eb
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-06-22 00:14:12 +0500

    Solved 2937 - Leetcode task.

--- src/main/java/tasks/MakeThreeStringsEqual2937.java
+++ src/main/java/tasks/MakeThreeStringsEqual2937.java
@@ -2,12 +2,19 @@ package tasks;
 
 public class MakeThreeStringsEqual2937 {
     public static void main(String[] args) {
-
+        System.out.println(findMinimumOperations("abc", "abb", "ab"));
+        System.out.println(findMinimumOperations("dac", "bac", "cac"));
     }
 
     public static int findMinimumOperations(String s1, String s2, String s3) {
+        int i = 0;
 
+        while ((s1.length() > i && s2.length() > i && s3.length() > i) && s1.charAt(i) == s2.charAt(i) && s1.charAt(i) == s3.charAt(i))
+            i++;
 
-        return -0;
+        if (i == 0)
+            return -1;
+
+        return s1.length() - i + s2.length() - i + s3.length() - i;
     }
 }
