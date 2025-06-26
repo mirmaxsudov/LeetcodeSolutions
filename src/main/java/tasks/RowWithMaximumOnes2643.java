9e82d09e5ede1b038eab122b9d709dd82f4b9132
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-06-26 22:50:25 +0500

    Solved 2643 - Leetcode task.

---
+++ src/main/java/tasks/RowWithMaximumOnes2643.java
@@ -0,0 +1,29 @@
+package tasks;
+
+public class RowWithMaximumOnes2643 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] rowAndMaximumOnes(int[][] mat) {
+        int max = 0;
+        int pos = -1;
+        z
+        int i = 0;
+        for (int[] nums : mat) {
+            int c = 0;
+            for (int num : nums) {
+                if (num == 1)
+                    c++;
+
+                if (c > max) {
+                    max = c;
+                    pos = i;
+                }
+            }
+            i++;
+        }
+
+        return new int[]{pos, max};
+    }
+}
