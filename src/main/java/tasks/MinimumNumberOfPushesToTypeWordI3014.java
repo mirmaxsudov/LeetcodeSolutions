b02fc717094e379e5e7501e1df6c67ea1243455f
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-10-27 22:43:01 +0500

    Task.

--- src/main/java/tasks/MinimumNumberOfPushesToTypeWordI3014.java
+++ src/main/java/tasks/MinimumNumberOfPushesToTypeWordI3014.java
@@ -1,47 +1,30 @@
 package tasks;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class MinimumNumberOfPushesToTypeWordI3014 {
     public static void main(String[] args) {
-
+        MinimumNumberOfPushesToTypeWordI3014 solution = new MinimumNumberOfPushesToTypeWordI3014();
+        System.out.println(solution.minimumPushes("abcde"));  // Should print 5
+        System.out.println(solution.minimumPushes("xycdefghij"));  // Should print 12
     }
 
-    public static int minimumPushes(String word) {
-        Map<Character, Integer> mp = new HashMap<>();
-        mp.put('a', 1);
-        mp.put('b', 2);
-        mp.put('c', 3);
-        mp.put('d', 1);
-        mp.put('e', 2);
-        mp.put('f', 3);
-        mp.put('g', 1);
-        mp.put('h', 2);
-        mp.put('i', 3);
-        mp.put('j', 1);
-        mp.put('k', 2);
-        mp.put('l', 3);
-        mp.put('m', 1);
-        mp.put('n', 2);
-        mp.put('o', 3);
-        mp.put('p', 1);
-        mp.put('q', 2);
-        mp.put('r', 3);
-        mp.put('s', 4);
-        mp.put('t', 1);
-        mp.put('u', 2);
-        mp.put('v', 3);
-        mp.put('w', 1);
-        mp.put('x', 2);
-        mp.put('y', 3);
-        mp.put('z', 4);
+    public int minimumPushes(String word) {
+        int[] chars = new int[26];
 
-        int sum = 0;
+        for (char c : word.toCharArray()) {
+            chars[c - 'a']++;
+        }
 
-        for (char c : word.toCharArray())
-            sum += mp.get(c);
+        Arrays.sort(chars);
 
-        return sum;
+        int res = 0;
+        for (int i = 25; i >= 0; i--) {
+            if (chars[i] == 0) continue;
+            int pushes = (25 - i) / 8 + 1;
+            res += chars[i] * pushes;
+        }
+
+        return res;
     }
 }
