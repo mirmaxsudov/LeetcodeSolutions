3eeba9fdbf3589b3d98e2f924968819bf7ec7648
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-11-25 11:27:11 +0500

    solved: Leetcode task - 2437

--- src/main/java/tasks/NumberOfValidClockTimes2437.java
+++ src/main/java/tasks/NumberOfValidClockTimes2437.java
@@ -8,9 +8,17 @@ public class NumberOfValidClockTimes2437 {
     public static int countTime(String time) {
         int res = 1;
 
-        if (time.startsWith("??")) {
-            res *= 24;
-        }
+        if (time.startsWith("??"))
+            res = 24;
+        else if (time.charAt(0) == '?')
+            res *= (time.charAt(1)) - '0' >= 4 ? 2 : 3;
+        else if (time.charAt(1) == '?')
+            res *= time.charAt(0) == '2' ? 4 : 10;
+
+        if (time.charAt(3) == '?')
+            res *= 6;
+        if (time.charAt(4) == '?')
+            res *= 10;
 
         return res;
     }
