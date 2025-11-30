361fa712fc16421c66422d2c73dbaa62163bc0ab
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-11-30 21:08:48 +0500

    x

---
+++ src/main/java/tasks/EarliestFinishTimeForLandAndWaterRidesI3633.java
@@ -0,0 +1,14 @@
+package tasks;
+
+public class EarliestFinishTimeForLandAndWaterRidesI3633 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int t = 0;
+        
+
+        return t;
+    }
+}
