c21ce90f166e21e18554d960f39a9848ce32b89f
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-12-20 11:54:25 +0500

    Solved 677th leetcode task.

--- src/main/java/tasks/EarliestFinishTimeForLandAndWaterRidesI3633.java
+++ src/main/java/tasks/EarliestFinishTimeForLandAndWaterRidesI3633.java
@@ -4,11 +4,4 @@ public class EarliestFinishTimeForLandAndWaterRidesI3633 {
     public static void main(String[] args) {
 
     }
-
-    public static int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int t = 0;
-        
-
-        return t;
-    }
 }
--- src/main/java/tasks/MapSumPairs677.java
+++ src/main/java/tasks/MapSumPairs677.java
@@ -2,17 +2,24 @@ package tasks;
 
 public class MapSumPairs677 {
     public static void main(String[] args) {
-
+        MapSum mapSum = new MapSum();
+        mapSum.insert("apple", 3);
+        System.out.println(mapSum.sum("apple"));
+        mapSum.insert("app", 2);
+        System.out.println(mapSum.sum("ap"));
     }
 
-    class MapSum {
+    static class MapSum {
         class Trie {
+            int sum;
             int val;
             Trie[] children;
             boolean isEndOfWord;
 
             public Trie() {
                 this.children = new Trie[26];
+                this.sum = 0;
+                this.val = 0;
             }
         }
 
@@ -20,40 +27,49 @@ public class MapSumPairs677 {
 
         public MapSum() {
             this.root = new Trie();
-            this.root.val = 0;
         }
 
         public void insert(String key, int val) {
             Trie temp = root;
-            for (int i = 0; i < key.length(); i++) {
-                char cha = key.charAt(i);
+            for (char c : key.toCharArray()) {
+                if (temp.children[c - 'a'] == null) {
+                    temp.children[c - 'a'] = new Trie();
+                }
+                temp = temp.children[c - 'a'];
+                Trie word = getWord(key);
+                if (word != null)
+                    temp.sum -= word.val;
 
-                if (temp.children[cha - 'a'] == null)
-                    temp.children[cha - 'a'] = new Trie();
-
-                temp = temp.children[cha - 'a'];
+                temp.sum += val;
             }
-
             temp.isEndOfWord = true;
             temp.val = val;
         }
 
-        public int sum(String prefix) {
-            int sum = 0;
+        private Trie getWord(String key) {
+            Trie temp = root;
 
+            for (char c : key.toCharArray()) {
+                if (temp.children[c - 'a'] == null)
+                    return null;
+                temp = temp.children[c - 'a'];
+            }
+
+            return temp.isEndOfWord ? temp : null;
+        }
+
+        public int sum(String prefix) {
             Trie temp = root;
 
             for (char c : prefix.toCharArray()) {
-                sum += temp.val;
-
-                if (temp.children[c - 'a'] != null) {
+                if (temp.children[c - 'a'] == null) {
+                    return 0;
+                } else {
                     temp = temp.children[c - 'a'];
-                    sum += temp.val;
-                } else
-                    break;
+                }
             }
 
-            return sum;
+            return temp.sum;
         }
     }
 }
