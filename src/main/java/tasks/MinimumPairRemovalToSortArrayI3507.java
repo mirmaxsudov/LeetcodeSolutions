c9648db4ebc02622c2316e8dcfb9645ee9a4ccda
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2026-01-04 23:55:21 +0500

    Commit

--- src/main/java/tasks/MinimumPairRemovalToSortArrayI3507.java
+++ src/main/java/tasks/MinimumPairRemovalToSortArrayI3507.java
@@ -6,38 +6,42 @@ import java.util.List;
 
 public class MinimumPairRemovalToSortArrayI3507 {
     public static void main(String[] args) {
-
+        System.out.println(minimumPairRemoval(new int[]{5, 2, 3, 1}));
     }
 
     public static int minimumPairRemoval(int[] nums) {
-        int minOp = 0;
-
+        int r = 0;
         List<Integer> list = new ArrayList<>();
 
         for (int num : nums)
             list.add(num);
 
         while (true) {
-            int num1 = 0, num2 = 0;
-            boolean isFind = false;
-            for (int i = list.size() - 1; i >= 1; i--) {
-                if (list.get(i) < list.get(i - 1)) {
-                    num1 = list.get(i);
-                    num2 = list.get(i - 1);
-                    isFind = true;
+            int minNum = Integer.MAX_VALUE;
+            int i1 = -1, i2 = -1;
+
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i) <= list.get(i + 1))
+                    continue;
+                int secondMin = list.get(i) + list.get(i + 1);
+
+                if (minNum == secondMin)
                     break;
+
+                if (minNum > secondMin) {
+                    i1 = i;
+                    i2 = i + 1;
+                    minNum = secondMin;
                 }
             }
 
-            if (isFind) {
-                list.removeLast();
-                list.set(list.size() - 1, num1 + num2);
-            } else {
-                break;
+            if (i1 == -1 || i2 == -1)
+                return r + 1;
+            else {
+                list.remove(i2);
+                list.remove(i1);
             }
-            minOp++;
+            r++;
         }
-
-        return minOp;
     }
 }
